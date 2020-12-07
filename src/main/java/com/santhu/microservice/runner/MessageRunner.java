a3c909d27b8e80f341dab349be78cc54e236472a
santhosh <santhoshty97@gmail.com>
2020-12-07 12:24:30 +0530

    WH - 988 Change in function

--- src/main/java/com/santhu/microservice/runner/MessageRunner.java
+++ src/main/java/com/santhu/microservice/runner/MessageRunner.java
@@ -9,7 +9,7 @@ public class MessageRunner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		System.out.println("Welcome to spring boot app! ");
+		int a = 10;
 		System.out.println("Hello Modify");
 	}
 
