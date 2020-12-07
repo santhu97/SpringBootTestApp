3a92da849c9b087b8a487b6ea9eec1cbee174e5a
santhosh <santhoshty97@gmail.com>
2020-12-07 11:50:23 +0530

    WH-8520 - First Time File Update

---
+++ src/main/java/com/santhu/microservice/runner/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.santhu.microservice.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to spring boot app! ");
+	}
+
+}
---
+++ src/test/java/com/santhu/microservice/SpringBootTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhu.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
